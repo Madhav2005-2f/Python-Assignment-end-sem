d2467b6c751a3e17869a3e518ad7d5e8ae0c3d51
Madhav Varshney <108249557+Madhav2005-2f@users.noreply.github.com>
2025-04-20 16:25:40 +0530

    Done

---
+++ Assignment File Handling/Ques1.py
@@ -0,0 +1,12 @@
+#Write a Python program to append text to a file and display the text.
+def append(filename):
+    text = input("Enter the text to append: ")
+    with open(filename, "a") as file:
+        file.write(text + "\n")
+        print("Text appended successfully.")
+def display(filename):
+    with open(filename, "r") as file:
+        print(file.read())
+file = input("Enter the file name: ")
+append(file)
+read(file)
